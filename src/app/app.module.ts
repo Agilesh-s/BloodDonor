04af95b9c61f8a8faca276a2006416aea7ceff81
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-08 12:48:28 +0530

    Search Donor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddDonorComponent } from './add-donor/add-donor.component';
+import { SearchDonorComponent } from './search-donor/search-donor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AddDonorComponent
+    AddDonorComponent,
+    SearchDonorComponent
   ],
   imports: [
     BrowserModule,
