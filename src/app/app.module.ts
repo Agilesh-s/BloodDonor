1ace9d355bc42712e4493e5f62a51b7572801a28
Agilesh-s <agileshsivakumar.s@gmail.com>
2022-10-08 12:33:18 +0530

    Add Donor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddDonorComponent } from './add-donor/add-donor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddDonorComponent
   ],
   imports: [
     BrowserModule,
